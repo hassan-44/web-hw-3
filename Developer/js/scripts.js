065c60164093fbef07e17d215e47809ad27ef039
Hassan Hussain <hassan@smart-itech.com>
2020-07-08 01:31:06 +0300

    homework 3

---
+++ Developer/js/scripts.js
@@ -0,0 +1,74 @@
+// const language = ["HTML", "CSS", "Java", "Javascript", "Python" ];
+
+// const want_language = language[2];
+
+// console.log("Welcome to our jobs website");
+
+// let name = prompt("What is your name?");
+
+// let age =  parseInt(prompt("How old are you?"));
+
+// let experience = parseInt(prompt("How many years of experience do you have?"));
+
+
+// console.log ("The Languages are:");
+
+// console.log("1. " + language[0]);
+
+// console.log("2. " + language[1]);
+
+// console.log("3. " + language[2]);
+
+// console.log("4. " + language[3]);
+
+// console.log("5. " + language[4]);
+
+// let exp1 = prompt("Choose a language");
+
+// let exp2 = prompt("Choose another language");
+
+
+
+// if (
+//     age > 25 &&
+//     age < 40 &&
+//     experience > 3 &&
+//     (exp1 == want_language || exp2 == want_language)
+// ) {
+//     console.log("You are accepted");
+// } else {
+//     console.log("You are rejected");
+// }
+
+
+
+
+// function logger( temperature_array) {
+//     for (let i = 0; i < temperature_array.length; i++){
+//         console.log(temperature_array[i]);
+//     }
+// } 
+
+// let array = [1, 2, 3];
+// logger(array);
+
+// let fer_temperature = [32, 70, 99, 106];
+
+// let hot = [];
+// let initial_value = 30
+// let result_array = [0, 21, 37, 41];
+
+// function hottsestdays(result_array, initial_value) {
+//     for (let i = 0; i < result_array.length; i++){
+//         if (result_array[i] > initial_value); {
+//             hot.push(result_array[i]);
+//         }
+//     }
+//     return hot;
+// }
+
+
+
+
+// I am stuck here and dont know how to continue the homework I hope this is enough thankyou.
+
